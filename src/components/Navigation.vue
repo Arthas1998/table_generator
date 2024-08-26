8ea5c15ff8408c92c622475171edf56223a85b48
wuweihang <wuweihang1998@foxmail.com>
2024-08-26 16:52:46 +0800

    header

---
+++ src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-tabs
+    v-model="activeName"
+    type="card"
+    class="demo-tabs"
+    @tab-click="handleClick"
+  >
+    <el-tab-pane label="User" name="first">User</el-tab-pane>
+    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
+    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      activeName: 'first',
+    }
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    },
+  },
+}
+</script>
+
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
