64dfbad224fa7532b14dc2939c6d12ae83dd6507
wuweihang <wuweihang1998@foxmail.com>
2024-09-27 18:19:21 +0800

    Remove redundant code

--- src/stores/spreadsheet.js
+++ src/stores/spreadsheet.js
@@ -1,42 +1,3 @@
-// import { defineStore } from 'pinia';
-// import { reactive, ref } from "vue";
-//
-// export const useSpreadsheetStore = defineStore('spreadsheet', {
-//   state: () => ({
-//     spreadsheetInstance: null
-//   }),
-//   actions: {
-//     setSpreadsheet(instance) {
-//       this.spreadsheetInstance = instance;
-//     },
-//     addRow() {
-//       this.spreadsheetInstance.insertRow();
-//     },
-//     addColumn() {
-//       this.spreadsheetInstance.insertColumn();
-//     },
-//     mergeCells(x1, y1, x2, y2) {
-//       this.spreadsheetInstance.setMerge(x1, y1, x2, y2);
-//     },
-//     refresh() {
-//       this.spreadsheetInstance.refresh();
-//     }
-//   }
-// });
-
-// export const useSpreadsheetStore = defineStore('spreadsheet', () => {
-//   const spreadsheetInstance = reactive(null)
-//   // function switchPage(index) {
-//   //   activeName.value = index;
-//   // }
-//
-//   return {
-//     spreadsheetInstance
-//   }
-// })
-
-
-// src/stores/spreadsheet.js
 import { defineStore } from 'pinia';
 
 export const useSpreadsheetStore = defineStore('spreadsheetstore', {
@@ -71,7 +32,7 @@ export const useSpreadsheetStore = defineStore('spreadsheetstore', {
           this.columns = this.spreadsheetInstance.options.columns;
           this.style = this.spreadsheetInstance.options.style;
       },
-      setSpreadsheetInstance(instance) {  // 新增方法来存储jspreadsheet实例
+      setSpreadsheetInstance(instance) {
         this.spreadsheetInstance = instance;
       },
   }
